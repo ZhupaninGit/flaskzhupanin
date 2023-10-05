af51fe10d7758ee65041d392ea3ad10235659e0e
ZhupaninGit <vitali.zhupanin@gmail.com>
2023-10-05 22:39:01 +0300

    Completed Task 1.2.

--- Laboratory work 1/Task 1/client.py
+++ Laboratory work 1/Task 1/client.py
@@ -2,15 +2,10 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect(('localhost', 33333))
-
-server_responce = s.recv(1024)
-print(server_responce.decode('utf-8'))
-
+serverresponce = s.recv(1024)
+print(serverresponce.decode('utf-8'))
 a = input()
 s.send(a.encode('utf-8'))
+serverresponce = s.recv(1024)
+print(serverresponce.decode('utf-8'))
 
-server_responce = s.recv(1024)
-print(server_responce.decode('utf-8'))
-
-
-s.close()
--- Laboratory work 1/Task 1/server.py
+++ Laboratory work 1/Task 1/server.py
@@ -1,19 +1,26 @@
 import socket
-import datetime
+import time
 
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-serversocket.bind(('localhost', 33333))
+serveraddress = ('localhost', 33333)
+serversocket.bind(serveraddress)
+
 serversocket.listen(5)
 
 print('Server waiting for the clients...')
-
 clientsocket, addr = serversocket.accept()
+
 print('Connection from :  {}'.format(addr))
 clientsocket.send('Enter your sentence : '.encode('utf-8'))
-sentence = clientsocket.recv(1024)
-now = datetime.datetime.now()
-print(sentence.decode('utf-8') + "\nTime of message : {}".format(now))
+data = clientsocket.recv(1024)
+time.sleep(5)
+if clientsocket.send(data) == len(data):
+    print(f"Sent back: {data.decode()}")
+else:
+    print("Sent error.")
+
 
 clientsocket.close()
+serversocket.close()
