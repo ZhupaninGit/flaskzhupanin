9e4276a013e8e3ed7a76a05e650f1b8a2f7c788b
ZhupaninGit <vitali.zhupanin@gmail.com>
2023-10-10 21:39:39 +0300

    Completed Task 2.

---
+++ Laboratory work 2/cgi-bin/delete_cookies.py
@@ -0,0 +1,24 @@
+import cgi
+import os
+import http.cookies
+
+headerString = (f"""Content-type: text/html\n
+    <title>Result</title>
+    <meta charset="UTF-8">
+    <script>alert("Cookies були видалені!Вас буде перенаправлено на початкову сторінку.");</script>
+    <meta http-equiv="refresh" content="0;url=http://localhost:9999/" />
+    <link rel="stylesheet" type="text/css" href="../styles/style.css">
+    <center>
+""")
+
+
+print("Set-Cookie: languageradio=This could be anything. The important part is the key name xml_edit_tool;expires=Wed, 28 Aug 2013 18:30:00 GMT")
+print("Set-Cookie: username=This could be anything;expires=Wed, 28 Aug 2013 18:30:00 GMT")
+print("Set-Cookie: language=anything;expires=Wed, 28 Aug 2013 18:30:00 GMT")
+print('Content-Type: text/html')
+redirectURL = "http://localhost:9999/"
+print(headerString)
+print('Location: %s' % redirectURL)
+
+print('Redirecting... <a href="%s">Click here if you are not redirected</a>' % redirectURL)
+
--- Laboratory work 2/cgi-bin/form_handler.py
+++ Laboratory work 2/cgi-bin/form_handler.py
@@ -1,4 +1,19 @@
 import cgi
+import os
+import http.cookies
+
+def getCookieValue(key):
+    if "HTTP_COOKIE" in os.environ:
+        cookie_string = os.environ.get("HTTP_COOKIE")
+        cookies = http.cookies.SimpleCookie()
+        cookies.load(cookie_string)
+        if key in cookies:
+            return cookies[key].value
+    return None
+
+def delete_cookie(cookie_name):
+    expired_date = 'expires=Wed, 28 Aug 2013 18:30:00 GMT'
+    print("Set-Cookie: %s=This could be anything. The important part is the key name xml_edit_tool; %s \r\n" % (cookie_name, expired_date))
 
 form = cgi.FieldStorage()
 
@@ -7,24 +22,43 @@ language = form.getvalue("language")
 language_radio = form.getvalue("language_radio")
 language_check = form.getlist("language_check")
 
-
-print("Content-type: text/html\n")
-print("""
+headerString = (f"""Content-type: text/html\n
     <title>Result</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="../styles/style.css">
     <center>
 """)
+
 if not username or not language or not language_check:
-    print("<h2 class='error'>Заповність всі поля,будь ласка!</h2></center>")
+    print(headerString)
+    print("<h2 class='error'>Заповність всі поля,будь ласка!</h2>")
+    print("<button class='buttonclass'><a style='text-decoration:none;color:white' href='http://localhost:9999/'>Повернутися до попередньої сторінки</a></button></center>")
+
 else:
+
+    print(f"Set-cookie: username={username};")
+    print(f"Set-cookie: language={language};")
+    print(f"Set-cookie: languageradio={language_radio};")
+
+    name_cookie = getCookieValue("username")
+    language_cookie = getCookieValue("language")
+    languageradio_cookie = getCookieValue("languageradio")
+
+
+    print(headerString)
     print(f"""
     <h2>Результат</h2>
     <p>Твоє ім'я : <span>{username}</span></p>
     <p>Твоя улюблена мова програмування : <span>{language}</span></p>
     <p>Між С++ та Python для парсингу ти обереш : <span>{language_radio}   
-    <p>Мови(-а) програмування , з якими ти працював : <span> {language_check}""")
-print("""
+    <p>Мови(-а) програмування , з якими ти працював : <span> {language_check}</p>
+    <p>Інформація з cookies : <span>username = {name_cookie},language = {language_cookie},languageradio = {languageradio_cookie}
+    <form action="delete_cookies.py">
+    <input class="buttonclass" type="submit" value="Видалити всі cookies">
+    </form>
+    """)
 
+
+print("""
 </center>
 """)
